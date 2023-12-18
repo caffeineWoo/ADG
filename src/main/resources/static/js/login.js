2e84f7f54bc1f4c6763800b6bb42ec6d4345b4d4
L0CUST <namae01@naver.com>
2023-12-18 18:24:39 +0900

    login 수정

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,7 +1,21 @@
 const login = () => {
-    let userId = document.getElementById('userId').value;
-    let userPassword = document.getElementById('userPassword').value;
+    event.preventDefault(); // Prevent the default form submission
 
-    console.log('userId: ', userId);
-    console.log('userPassword: ', userPassword);
+    // Get the form data (you may use FormData or other methods depending on your setup)
+    const formData = {
+        MemberEmail: document.getElementById('userId').value,
+        MemberPassword: document.getElementById('userPassword').value
+    };
+
+    // Perform your login logic, e.g., send a request to the server
+    // Assume a successful login for demonstration purposes
+    const loginSuccessful = true;
+
+    if (loginSuccessful) {
+        // Redirect to the desired URL
+        window.location.href = '/ADG/document';
+    } else {
+        // Handle unsuccessful login (show an error message, etc.)
+        console.log('Login failed');
+    }
 }

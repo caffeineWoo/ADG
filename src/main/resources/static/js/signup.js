a323676f65236f8dcc4d32b869483ea081c6e158
L0CUST <namae01@naver.com>
2023-12-21 22:13:53 +0900

    수정

--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -1,9 +1,26 @@
-const register = () => {
-    let userId = document.getElementById('userId').value;
-    let userPassword = document.getElementById('userPassword').value;
-    let userName = document.getElementById('userName').value;
+// Assuming you have a function called register() for validation
+function register() {
+    // Your validation logic goes here
+    // Example: Check if the email is valid
+    const email = document.getElementById('userId').value;
+    const emailRegex = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+    const isUserValid = emailRegex.test(email);
 
-    console.log('userId: ', userId);
-    console.log('userPassword: ', userPassword);
-    console.log('userName: ', userName);
+    // Enable or disable the button based on the validation result
+    const registerButton = document.getElementById('register_btn');
+    registerButton.disabled = !isUserValid;
+
+    // Change style based on validation result
+    if (!isUserValid) {
+        registerButton.style.backgroundColor = '#999';
+        registerButton.style.color = 'white';
+        // You can add more styling properties here
+    } else {
+        // Reset styles to default when the input is valid
+        registerButton.style.backgroundColor = '';
+        registerButton.style.color = '';
+    }
 }
+
+// Attach the register() function to the input elements' events for real-time validation
+document.getElementById('userId').addEventListener('input', register);
